704e4b5b0104237dcc9f2a4f63a32155a0b0fce1
Aerin2805 <107164788+Aerin2805@users.noreply.github.com>
2023-06-06 21:27:49 +0530

    day-1✅

---
+++ day-1/Next Permutation.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h> 
+vector<int> nextPermutation(vector<int> &p, int n)
+{
+    //  Write your code here.
+ int index = -1;
+        for(int i=n-2; i>=0; i--){
+            if(p[i] < p[i+1]){
+                index = i;
+                break;
+            }
+        }
+        for(int i=n-1; i>=index && index != -1; i--){
+            if(p[i] > p[index]){
+                swap(p[i], p[index]);
+                break;
+            }
+        }
+        reverse(p.begin() + index + 1, p.end());
+
+        return p;
+}
