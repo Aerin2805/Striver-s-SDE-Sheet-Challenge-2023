1e3c7b9dfb376842062e52de282efd6de61dc245
Aerin2805 <107164788+Aerin2805@users.noreply.github.com>
2023-06-06 20:29:08 +0530

    question_1

---
+++ day-1/Set Matrix Zeros.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+void setZeros(vector<vector<int>> &mat)
+{
+	// Write your code here.
+	int n = mat.size();
+	int m = mat[0].size();
+
+	unordered_map<int,bool>mr;
+	unordered_map<int,bool>mc;
+
+	for(int i = 0 ; i<n ; i++){
+		for(int j = 0 ; j<m ; j++){
+			if(mat[i][j]==0){
+				mr[i] = true;
+				mc[j] = true;
+			}
+		}
+	}
+	for(int i = 0 ; i<n ; i++){
+		for(int j = 0 ; j<m ; j++){
+			if(mr.count(i) || mc.count(j)){
+			  mat[i][j]=0;
+			}
+		}
+	}
+}
