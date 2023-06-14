7ea280b84aad5ca85f2eaa1d46d6791bc8b0d8f2
Aerin2805 <107164788+Aerin2805@users.noreply.github.com>
2023-06-14 19:38:43 +0530

    day-5 completed✅

--- LINKED-LIST/day-5/Delete Node in a Linked List.cpp
+++ LINKED-LIST/day-5/Delete Node in a Linked List.cpp
@@ -1,15 +1,26 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
- * };
- */
-class Solution {
-public:
-    void deleteNode(ListNode* node) {
-        node->val = node->next->val;
-        node->next = node->next->next;
-    }
-};
+#include <bits/stdc++.h>
+
+/****************************************************************
+
+    Following is the class structure of the LinkedListNode class:
+
+    template <typename T>
+    class LinkedListNode
+    {
+    public:
+        T data;
+        LinkedListNode<T> *next;
+        LinkedListNode(T data)
+        {
+            this->data = data;
+            this->next = NULL;
+        }
+    };
+
+*****************************************************************/
+
+void deleteNode(LinkedListNode<int> * node) {
+    // Write your code here.
+    node->data = node->next->data;
+    node->next = node->next->next;
+}
